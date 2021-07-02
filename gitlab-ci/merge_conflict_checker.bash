3e6408739a3acfff9159863817fcbf578d250bb5
Melinheim <61503704+Melinheim@users.noreply.github.com>
2021-07-02 10:33:49 +0300

    Delete merge_conflict_checker.bash

--- gitlab-ci/merge_conflict_checker.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-all_conflics=$(grep --exclude-dir=.git --exclude=\*.git* -rnw './' -e '<<<<<<< HEAD') || true
-echo $all_conflics
-output=$(echo $all_conflics | wc -l)
-empty_var=$( [ -z "$all_conflics" ] && echo 1 || echo 0)
-if [ $output -ne 0 -a $empty_var -ne 1 ]; then echo "solve merge conflicts please"; false; fi;
